6db512ac6271215cab78090a464d6493c4842903
bruce(lixun) <bruce.lixun@gmail.com>
2020-02-10 23:04:16 +0800

    优化注入常量配置，增加common常量

--- webpack.constants.js
+++ webpack.constants.js
@@ -1,38 +1,43 @@
-const constants = {
+/**
+ * 注意：
+ * - 请不要在另外的文件中管理需在编译时注入的全局常量。
+ *
+ * 原理：
+ * - 使用webpack.DefinePlugin在编译时注入全局常量。
+ *
+ * 用法：
+ * - 不依赖配置环境的全局常量写在common中。
+ * - 依赖配置环境的全局常量写在constants中。
+ * - 导出时，common和constants会合并在一起。
+ * - 建议常量名称前带上 WC_ 前缀（webpack constant缩写），以区分代码中不同的常量类型。
+ */
+
+const common = {
     /**
-     * 需要使用webpack.DefinePlugin在编译时注入的全局常量
-     *
-     * 注意：
-     * - 请不要在另外的文件里去管理需要根据不同环境改变的全局常量。
-     *
      * 常见问题：
-     * - 其实JSON.stringify不是必须，参见下面的 WC_DEMO 和 WC_DEMO5_IS_DEV。
+     * - JSON.stringify不是必须的，例如 WC_DEMO 和 WC_DEMO2 的写法是一样的。
+     * - 非字符串是不用JSON.stringify的。
      * - 参考文档 https://webpack.js.org/plugins/define-plugin/#usage
-     *
-     * 使用：
-     * - dev, test, prod是指需要在哪个环境下注入常量，通过env.constEnv设置。例：webpack --env.constEnv=test
-     * - 你也可以添加更多的环境，也可以自己取名，而不用dev, test和prod。只要env.constEnv的值跟你起的名字对上就行。
-     * - 建议常量名称前带上 WC_ 前缀（webpack constant缩写），以区分代码中不同的常量类型。
      */
-    // dev: {
-    //     WC_DEMO: '"Hello Dev"',
-    //     WC_DEMO2: JSON.stringify('Hello Dev'),
-    //     WC_DEMO3_URL: '"http://www.dev.com"',
-    //     WC_DEMO4_HOST: '"www.dev.com"',
-    //     WC_DEMO5_IS_DEV: true, // 非字符串是不用JSON.stringify的
-    // },
-    // test: {
-    //     WC_DEMO: '"Hello Test"',
-    //     WC_DEMO2: JSON.stringify('Hello Test'),
-    //     WC_DEMO3_URL: '"http://www.test.com"',
-    //     WC_DEMO4_HOST: '"www.test.com"',
-    // },
-    // prod: {
-    //     WC_DEMO: '"Hello Prod"',
-    //     WC_DEMO2: JSON.stringify('Hello Prod'),
-    //     WC_DEMO3_URL: '"http://www.prod.com"',
-    //     WC_DEMO4_HOST: '"www.prod.com"',
-    // }
+    // WC_DEMO: '"Hello Dev"',
+    // WC_DEMO2: JSON.stringify('Hello Dev'),
 }
 
-module.exports = ({constEnv}) => constants[constEnv] || {}
+const constants = {
+    /**
+     * 使用：
+     * - dev, test, prod是指需要在哪个配置环境下注入常量，通过env.constEnv设置。例：webpack --env.constEnv=test
+     * - 你可以添加更多的配置环境，也可以自己取名，而不用dev、test和prod，只要env.constEnv的值跟你起的名字对上就行。
+     */
+    dev: {
+        // WC_DEMO3_URL: '"http://www.dev.com"',
+    },
+    test: {
+        // WC_DEMO3_URL: '"http://www.test.com"',
+    },
+    prod: {
+        // WC_DEMO3_URL: '"http://www.prod.com"',
+    }
+}
+
+module.exports = ({constEnv}) => ({...common, ...(constants[constEnv] || {})})
